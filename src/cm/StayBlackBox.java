4b827208b63898d2aaaa8d6453894e69f3faa024
mantasmacionis <mm.macionis@gmail.com>
2024-08-16 12:25:00 +0100

    Stay Black Box test class initial commit
    Class which will contain black box tests for the stay class

---
+++ src/cm/StayBlackBox.java
@@ -0,0 +1,10 @@
+package cm;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+public class StayBlackBox {
+
+}
