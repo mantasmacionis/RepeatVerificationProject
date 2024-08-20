67cd309438ed54e2946ac1551af2140a2a853511
mantasmacionis <mm.macionis@gmail.com>
2024-08-20 12:44:40 +0100

    Implemented stay class constructor with parameter validation
    Stay class constructor which includes validation for all input parameters.
    constructor ensures none of the inputs: (entryGate, exitGate, entryDateTime, exitDateTime, charge, carParkValidator) are null
    It also validates that entryDateTime is before exitDateTime, charge is non negative, and the entryGate and exitGate belong to the same carPark if theyre different.

--- src/cm/Stay.java
+++ src/cm/Stay.java
@@ -6,4 +6,32 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Stay {
+
+    private Gate entryGate;
+    private Gate exitGate;
+    private LocalDateTime entryDateTime;
+    private LocalDateTime exitDateTime;
+    private BigDecimal charge;
+    private CarParkValidator carParkValidator;
+
+    public Stay(Gate entryGate, Gate exitGate, LocalDateTime entryDateTime, LocalDateTime exitDateTime, BigDecimal charge, CarParkValidator carParkValidator) {
+        if (entryGate == null || exitGate == null || entryDateTime == null || exitDateTime == null || charge == null) {
+            throw new NullPointerException("Arguments cannot be null");
+        }
+        if (entryDateTime.isAfter(exitDateTime)) {
+            throw new IllegalArgumentException("Entry date time must be before exit date time");
+        }
+        if (charge.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Charge must be non-negative");
+        }
+        if (!carParkValidator.belongToSameCarPark(entryGate, exitGate)) {
+            throw new IllegalArgumentException("Entry and exit gates must belong to the same CarPark if they are different");
+        }
+        this.entryGate = entryGate;
+        this.exitGate = exitGate;
+        this.entryDateTime = entryDateTime;
+        this.exitDateTime = exitDateTime;
+        this.charge = charge;
+        this.carParkValidator = carParkValidator;
+    }
 }
