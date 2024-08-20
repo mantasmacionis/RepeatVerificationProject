237af4fef3d6e65d4ebd94c47bcbc085ece439a6
mantasmacionis <mm.macionis@gmail.com>
2024-08-20 16:59:51 +0100

    White box test suite initial commit

---
+++ src/cm/MyTestSuite.java
@@ -0,0 +1,10 @@
+package cm;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTestSuite {
+}
